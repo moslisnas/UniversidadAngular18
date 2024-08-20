85af0411aba0c8a96a8a7e15a8beacb3c2e7a8c9
moslisnas <molicapacidad@gmail.com>
2024-08-21 00:34:52 +0200

    Configure main components of control-clientes

--- control-clientes/src/app/app.component.ts
+++ control-clientes/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CabeceroComponent } from "./componentes/cabecero/cabecero.component";
+import { PiePaginaComponent } from "./componentes/pie-pagina/pie-pagina.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CabeceroComponent, PiePaginaComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- control-clientes/src/app/componentes/cabecero/cabecero.component.ts
+++ control-clientes/src/app/componentes/cabecero/cabecero.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-cabecero',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './cabecero.component.html',
   styleUrl: './cabecero.component.css'
 })
